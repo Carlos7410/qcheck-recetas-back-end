59ff3f8b03125bbc7b05a99dfb018717e4c5f59d
Carlos7410 <carlos_rivero7410@hotmail.com>
2025-08-22 23:14:51 -0300

    initial commit

---
+++ src/main/java/com/qcrecetas/QcrecetasApplication.java
@@ -0,0 +1,13 @@
+package com.qcrecetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcrecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QcrecetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qcrecetas/QcrecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qcrecetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcrecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
